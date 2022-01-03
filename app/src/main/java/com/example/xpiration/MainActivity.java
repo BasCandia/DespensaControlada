f319eeaeedb46a1cedf8be5e44a60e194e139990
BasCandia <bcandia.0123@gmail.com>
2022-01-03 18:45:23 -0300

    Cambios de dia 1

--- app/src/main/java/com/example/xpiration/MainActivity.java
+++ app/src/main/java/com/example/xpiration/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.xpiration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ImageView casa;
+    ImageView mas;
+    ImageView menu;
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+
+        casa = findViewById(R.id.Casa);
+        casa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SegundaActivity.class);
+                context.startActivity(intent);
+            }
+        });
+
+        mas = findViewById(R.id.plus);
+        mas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SegundaActivity.class);
+                context.startActivity(intent);
+            }
+        });
+
+        menu = findViewById(R.id.Menu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SegundaActivity.class);
+                context.startActivity(intent);
+            }
+        });
+
     }
 }
