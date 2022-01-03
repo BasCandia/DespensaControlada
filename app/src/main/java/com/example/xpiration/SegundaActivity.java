f91f59d300d51fba9a2e88a738a6478ad57dc84d
matin <matifcvega@gmail.com>
2022-01-03 19:38:51 -0300

    agregar date picker a fecha de caducidad

--- app/src/main/java/com/example/xpiration/SegundaActivity.java
+++ app/src/main/java/com/example/xpiration/SegundaActivity.java
@@ -2,13 +2,58 @@ package com.example.xpiration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SegundaActivity extends AppCompatActivity {
+
+    EditText fecha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+
+
+        fecha = findViewById(R.id.FechaCaducidad);
+
+        fecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.FechaCaducidad:
+                        showDatePickerDialog();
+
+                        break;
+                }
+
+            }
+        });
+
     }
+
+
+    private void showDatePickerDialog() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // +1 because January is zero
+                final String selectedDate = day + " / " + (month+1) + " / " + year;
+                fecha.setText(selectedDate);
+            }
+        });
+
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
+
+
 }
+
+
+
