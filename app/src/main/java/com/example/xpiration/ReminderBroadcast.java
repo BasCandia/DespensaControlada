99e626975148e3a55b5316589392968325e97c8f
BasCandia <bcandia.0123@gmail.com>
2022-01-21 18:07:22 -0300

    Notificacion

--- app/src/main/java/com/example/xpiration/ReminderBroadcast.java
+++ app/src/main/java/com/example/xpiration/ReminderBroadcast.java
@@ -1,5 +1,7 @@
 package com.example.xpiration;
 
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -11,11 +13,21 @@ public class ReminderBroadcast extends BroadcastReceiver {
 //****************************** Clase para crear la notificacion **********************************
     @Override
     public void onReceive(Context context, Intent intent) {
+        PendingIntent pendingIntent;
+        Intent intentB = new Intent(context,SegundaActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(SegundaActivity.class);
+        stackBuilder.addNextIntent(intentB);
+        pendingIntent = stackBuilder.getPendingIntent(1,PendingIntent.FLAG_UPDATE_CURRENT);
+
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"ChannelXpiration")
                 .setSmallIcon(R.drawable.ic_apple_alt)
                 .setContentTitle("Xpiration")
                 .setContentText("Recuerda revisar la lista de cosas a expirar")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        builder.setContentIntent(pendingIntent);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         notificationManager.notify(200,builder.build());
