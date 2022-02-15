9513793d619bea4213c508fd33882ea250383750
matin <matifcvega@gmail.com>
2022-02-15 18:13:23 -0300

    Comentarios en lotes y productos

--- app/src/main/java/entidades/Lotes.java
+++ app/src/main/java/entidades/Lotes.java
@@ -105,21 +105,18 @@ public class Lotes {
         this.LOTE_EN_MERMA = LOTE_EN_MERMA;
     }
 
+    //******** Funcion para mostrar lotes correspondientes a un producto en base a un ID ***********
     public ArrayList<Lotes> mostrarLotes(int id){
 
         ArrayList<Lotes> listaLotes = new ArrayList<>();
         try {
             Lotes l;
             String query1 = "SELECT L.*,P.PRODUCTO_NOMBRE FROM LOTE L, PRODUCTO P WHERE L.PRODUCTO_ID = P.PRODUCTO_ID AND L.PRODUCTO_ID ="+ id + " ORDER BY LOTE_FECHA_CADUCIDAD ASC" ;
-            //SELECT L.*,P.PRODUCTO_NOMBRE FROM LOTE L, PRODUCTO P WHERE L.PRODUCTO_ID = P.PRODUCTO_ID AND L.PRODUCTO_ID = 1 ORDER BY LOTE_FECHA_CADUCIDAD ASC
-
             ConnectionHelper conexion = new ConnectionHelper();
             con = conexion.connectionclass();
             Statement st = null;
-
             st = con.createStatement();
             ResultSet rs = st.executeQuery(query1);
-
 
             while (rs.next()) {
                 l = new Lotes();
@@ -133,7 +130,6 @@ public class Lotes {
                 l.setPRODUCTO_NOMBRE(rs.getString("PRODUCTO_NOMBRE"));
                 l.setLOTE_EN_MERMA(rs.getString("LOTE_EN_MERMA").charAt(0));
                 listaLotes.add(l);
-
             }
         }catch (SQLException e){
             System.out.println("Error in sql statment");
@@ -144,71 +140,13 @@ public class Lotes {
 //************************ Funcion para insertar query en base de datos ****************************
     public void insertar (Context context,String idproducto, String n, String fechaIngreso, String fechaCaducidad, String notiNaranja, String notiRoja){
 //******************** Validaciones para query de insercion ****************************************
-            DateFormat dateFormatYMD = new SimpleDateFormat("yyyy/MM/dd");
-            Date nuevo = null;
-            Date comparado = null;
-
-
-            String nombre = n.replaceAll("[-+.^:,'(){}/]","");
-
-            if(fechaCaducidad.isEmpty()){
-                Toast.makeText(context,"Ingrese una fecha de vencimiento",Toast.LENGTH_SHORT).show();
-            }else {
-                try {
-                    nuevo = dateFormatYMD.parse(fechaIngreso);
-                    comparado = dateFormatYMD.parse(fechaCaducidad);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-                if (comparado.compareTo(nuevo) <= -2) {
-                    Toast.makeText(context, "El producto caduca hoy o ya ha caducado", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (nombre.length() == 0) {
-                        Toast.makeText(context, "Ingrese Nombre del producto", Toast.LENGTH_SHORT).show();
-                    } else {
-                        if((notiNaranja.isEmpty()) || (notiNaranja.compareToIgnoreCase("0") == 0)){
-                            Toast.makeText(context, "Ingrese dias para notificacion Preventiva", Toast.LENGTH_SHORT).show();
-                        }else{
-                            if((notiRoja.isEmpty()) || (notiRoja.compareToIgnoreCase("0") == 0)){
-                                Toast.makeText(context, "Ingrese dias para notificacion Critica", Toast.LENGTH_SHORT).show();
-                            }else{
-//************** inicializacion de conexion, construccion y envio de query *************************
-                                try {
-                                    String query = "INSERT LOTE VALUES (" + idproducto + ",'" + nombre +"','"+ fechaIngreso + "','" + fechaCaducidad + "',"+notiNaranja+','+notiRoja+",'N');";
-                                    ConnectionHelper conexion = new ConnectionHelper();
-                                    Connection con = conexion.connectionclass();
-
-                                    PreparedStatement pst = con.prepareStatement(query);
-
-                                    pst.executeUpdate();
-
-                                    Toast.makeText(context, "Lote agregado correctamente", Toast.LENGTH_SHORT).show();
-
-                                    Intent intent = new Intent(context, MainActivity.class);
-                                    context.startActivity(intent);
-
-
-                                } catch (SQLException e) {
-                                    Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        }
-
-                    }
-                }
-            }
-    }
-
-//************************ Funcion para editar query en base de datos ******************************
-    public static boolean Editar(Context context, int ID, String n, String fechaIngreso, String fechaCaducidad, String notiNaranja, String notiRoja){
-//******************** Validaciones para query de edicion ******************************************
         DateFormat dateFormatYMD = new SimpleDateFormat("yyyy/MM/dd");
         Date nuevo = null;
         Date comparado = null;
 
         String nombre = n.replaceAll("[-+.^:,'(){}/]","");
 
-        if(fechaCaducidad.isEmpty()){
+        if(fechaCaducidad.isEmpty()){ //Validar Fecha de caducidad vacia
             Toast.makeText(context,"Ingrese una fecha de vencimiento",Toast.LENGTH_SHORT).show();
         }else {
             try {
@@ -217,7 +155,56 @@ public class Lotes {
             } catch (ParseException e) {
                 e.printStackTrace();
             }
-            if (comparado.compareTo(nuevo) <= -2) {
+            if (comparado.compareTo(nuevo) <= -2) {  //Validar Fecha de caducidad menor a 2 dias (Se elimina de forma automatica si los productos pasan de los 3 dias de caducado)
+                Toast.makeText(context, "El producto caduca hoy o ya ha caducado", Toast.LENGTH_SHORT).show();
+            } else {
+                if (nombre.length() == 0) {
+                    Toast.makeText(context, "Ingrese Nombre del producto", Toast.LENGTH_SHORT).show();
+                } else {
+                    if((notiNaranja.isEmpty()) || (notiNaranja.compareToIgnoreCase("0") == 0)){
+                        Toast.makeText(context, "Ingrese dias para notificacion Preventiva", Toast.LENGTH_SHORT).show();
+                    }else{
+                        if((notiRoja.isEmpty()) || (notiRoja.compareToIgnoreCase("0") == 0)){
+                            Toast.makeText(context, "Ingrese dias para notificacion Critica", Toast.LENGTH_SHORT).show();
+                        }else{
+    //************ inicializacion de conexion, construccion y envio de query ***********************
+                            try {
+                                String query = "INSERT LOTE VALUES (" + idproducto + ",'" + nombre +"','"+ fechaIngreso + "','" + fechaCaducidad + "',"+notiNaranja+','+notiRoja+",'N');";
+                                ConnectionHelper conexion = new ConnectionHelper();
+                                Connection con = conexion.connectionclass();
+                                PreparedStatement pst = con.prepareStatement(query);
+                                pst.executeUpdate();
+                                Toast.makeText(context, "Lote agregado correctamente", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(context, MainActivity.class);
+                                context.startActivity(intent);
+                            } catch (SQLException e) {
+                                Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+//************************ Funcion para editar query en base de datos ******************************
+    public static boolean Editar(Context context, int ID, String n, String fechaIngreso, String fechaCaducidad, String notiNaranja, String notiRoja){
+//******************** Validaciones para query de edicion ******************************************
+        DateFormat dateFormatYMD = new SimpleDateFormat("yyyy/MM/dd");
+        Date nuevo = null;
+        Date comparado = null;
+        String nombre = n.replaceAll("[-+.^:,'(){}/]","");
+
+        if(fechaCaducidad.isEmpty()){ //Validar Fecha de caducidad vacia
+            Toast.makeText(context,"Ingrese una fecha de vencimiento",Toast.LENGTH_SHORT).show();
+        }else {
+            try {
+                nuevo = dateFormatYMD.parse(fechaIngreso);
+                comparado = dateFormatYMD.parse(fechaCaducidad);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            if (comparado.compareTo(nuevo) <= -2) { //Validar Fecha de caducidad menor a 2 dias (Se elimina de forma automatica si los productos pasan de los 3 dias de caducado)
                 Toast.makeText(context, "El producto caduca hoy o ya ha caducado", Toast.LENGTH_SHORT).show();
             } else {
                 if (nombre.length() == 0) {
@@ -234,50 +221,38 @@ public class Lotes {
                                 String query = (" UPDATE LOTE SET LOTE_NOMBRE = '" + nombre + "', LOTE_FECHA_INGRESO = '" + fechaIngreso + "', LOTE_FECHA_CADUCIDAD = '" + fechaCaducidad + "', LOTE_NOTIFICACION_NARANJA = " + notiNaranja + ", LOTE_NOTIFICACION_ROJA = " + notiRoja + "WHERE LOTE_ID = " + ID);
                                 ConnectionHelper conexion = new ConnectionHelper();
                                 Connection con = conexion.connectionclass();
-
                                 PreparedStatement pst = con.prepareStatement(query);
-
                                 pst.executeUpdate();
-
                                 Toast.makeText(context, "Lote editado correctamente", Toast.LENGTH_SHORT).show();
-
                                 Intent intent = new Intent(context, MainActivity.class);
                                 context.startActivity(intent);
-
                                 return true;
-
-
                             } catch (SQLException e) {
                                 Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
                                 return false;
                             }
                         }
                     }
-
                 }
             }
         }
         return false;
     }
 
+    //****************************** Retorna un Lote en base a un ID *******************************
     public Lotes verLote(int id){
         Lotes l = null;
         try {
-
             String query1 = "SELECT L.*,P.PRODUCTO_NOMBRE FROM LOTE L,PRODUCTO P WHERE L.PRODUCTO_ID = P.PRODUCTO_ID AND L.LOTE_ID =" + id;
-
             ConnectionHelper conexion = new ConnectionHelper();
             con = conexion.connectionclass();
             Statement st = null;
-
             st = con.createStatement();
             ResultSet rs = st.executeQuery(query1);
 
             while (rs.next()) {
                 l = new Lotes();
-                //l.setPRODUCTO_ID(rs.getInt("PRODUCTO_ID"));
                 l.setLOTE_NOMBRE(rs.getString("LOTE_NOMBRE"));
-                //l.setLOTE_FECHA_INGRESO(rs.getDate("LOTE_FECHA_INGRESO"));
                 l.setLOTE_FECHA_CADUCIDAD(rs.getDate("LOTE_FECHA_CADUCIDAD"));
                 l.setLOTE_NOTIFICACION_NARANJA(rs.getInt("LOTE_NOTIFICACION_NARANJA"));
                 l.setLOTE_NOTIFICACION_ROJA(rs.getInt("LOTE_NOTIFICACION_ROJA"));
@@ -291,20 +266,17 @@ public class Lotes {
         return l;
     }
 
+    //Retorna La lista de todos los lotes con una fecha de caducidad inferior a su respectiva notificacion preventiva (7 por defecto)
     public ArrayList<Lotes> mostrarTodoLotes(){
-
         ArrayList<Lotes> listaLotes = new ArrayList<>();
         try {
             Lotes l;
             String query1 = "SELECT L.*,P.PRODUCTO_NOMBRE FROM LOTE L, PRODUCTO P WHERE L.PRODUCTO_ID = P.PRODUCTO_ID AND DATEDIFF(DAY,GETDATE(),L.LOTE_FECHA_CADUCIDAD) <= LOTE_NOTIFICACION_NARANJA ORDER BY LOTE_FECHA_CADUCIDAD ASC" ;
-
             ConnectionHelper conexion = new ConnectionHelper();
             con = conexion.connectionclass();
             Statement st = null;
-
             st = con.createStatement();
             ResultSet rs = st.executeQuery(query1);
-
 
             while (rs.next()) {
                 l = new Lotes();
@@ -318,7 +290,6 @@ public class Lotes {
                 l.setPRODUCTO_NOMBRE(rs.getString("PRODUCTO_NOMBRE"));
                 l.setLOTE_EN_MERMA(rs.getString("LOTE_EN_MERMA").charAt(0));
                 listaLotes.add(l);
-
             }
         }catch (SQLException e){
             System.out.println("Error in sql statment");
@@ -326,21 +297,18 @@ public class Lotes {
         return listaLotes;
     }
 
-
+    //****************************** Retorna todos los Lotes en Merma ******************************
     public ArrayList<Lotes> MermaLotes(){
 
         ArrayList<Lotes> listaLotes = new ArrayList<>();
         try {
             Lotes l;
             String query1 = "SELECT L.*,P.PRODUCTO_NOMBRE FROM LOTE L, PRODUCTO P WHERE L.LOTE_EN_MERMA = 'S' AND  L.PRODUCTO_ID = P.PRODUCTO_ID AND DATEDIFF(DAY,GETDATE(),L.LOTE_FECHA_CADUCIDAD) <= LOTE_NOTIFICACION_NARANJA ORDER BY LOTE_FECHA_CADUCIDAD ASC\n" ;
-
             ConnectionHelper conexion = new ConnectionHelper();
             con = conexion.connectionclass();
             Statement st = null;
-
             st = con.createStatement();
             ResultSet rs = st.executeQuery(query1);
-
 
             while (rs.next()) {
                 l = new Lotes();
@@ -362,7 +330,7 @@ public class Lotes {
         return listaLotes;
     }
 
-//************************ Funcion para borrar query en base de datos ******************************
+    //********************** Funcion para borrar Un lote en base de datos **************************
     public boolean Borrar(int ID){
         boolean bandera;
         try{
@@ -372,30 +340,23 @@ public class Lotes {
             Statement st = con.createStatement();
             ResultSet rs = st.executeQuery(query);
             bandera = true;
-
         }catch (SQLException e) {
             bandera = false;
         }
-
-
         return bandera;
     }
 
+    //************************* Funcion para ingresar un Lote a Merma ******************************
     public void Mermar(int ID){
         try{
             String query = "UPDATE LOTE SET LOTE_EN_MERMA = 'S' WHERE LOTE_ID="+ID;
             ConnectionHelper conexion = new ConnectionHelper();
             Connection con = conexion.connectionclass();
-
             PreparedStatement pst = con.prepareStatement(query);
-
             pst.executeUpdate();
-
-
 
         }catch (SQLException e) {
             //Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
-
         }
     }
 
--- app/src/main/java/entidades/Productos.java
+++ app/src/main/java/entidades/Productos.java
@@ -30,7 +30,6 @@ public class Productos {
     //private int PRODUCTO_NOTIFICACION_NARANJA; //preventiva
     //private int PRODUCTO_NOTIFICACION_ROJA; //critica
 
-
     Connection con;
 
 //************************** Getter and setter de productos ****************************************
@@ -100,16 +99,11 @@ public class Productos {
                    String query = "INSERT PRODUCTO VALUES (" + Categoria + ",'" + nombre +"',null,'"+fechaIngreso+"');";
                    ConnectionHelper conexion = new ConnectionHelper();
                    Connection con = conexion.connectionclass();
-
                    PreparedStatement pst = con.prepareStatement(query);
-
                    pst.executeUpdate();
-
                    Toast.makeText(context, "Producto agregado correctamente", Toast.LENGTH_SHORT).show();
-
                    Intent intent = new Intent(context, MainActivity.class);
                    context.startActivity(intent);
-
 
                } catch (SQLException e) {
                    Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
@@ -118,20 +112,15 @@ public class Productos {
         }
     }
 
-
-
 //************************ Funcion para editar query en base de datos ****************************
     public boolean Editar (Context context,int ID, String n, String fechaIngreso, String fechaCaducidad,int Categoria, String notiNaranja, String notiRoja){
         //******************** Validaciones para query de edicion ****************************************
-
         DateFormat dateFormatYMD = new SimpleDateFormat("yyyy/MM/dd");
         Date nuevo = null;
         Date comparado = null;
 
-
         //validar el no ingreso de caracteres especiales
         String nombre = n.replaceAll("[-+.^:,'(){}/]","");
-
 
         if( Categoria == 0) {
             Toast.makeText(context,"Seleccione una Categoria",Toast.LENGTH_SHORT).show();
@@ -160,34 +149,27 @@ public class Productos {
 //************** inicializacion de conexion, construccion y envio de query *************************
                                 try {
                                     String query = (" UPDATE PRODUCTO SET CATEGORIA_ID = " + Categoria + ", PRODUCTO_NOMBRE = '" + nombre + "', PRODUCTO_FECHA_INGRESO = '" + fechaIngreso + "', PRODUCTO_FECHA_CADUCIDAD = '" + fechaCaducidad + "', PRODUCTO_NOTIFICACION_NARANJA = " + notiNaranja + ", PRODUCTO_NOTIFICACION_ROJA = " + notiRoja + "WHERE PRODUCTO_ID = " + ID);
-
                                     ConnectionHelper conexion = new ConnectionHelper();
                                     Connection con = conexion.connectionclass();
-
                                     PreparedStatement pst = con.prepareStatement(query);
-
                                     pst.executeUpdate();
-
                                     Toast.makeText(context, "Producto agregado correctamente", Toast.LENGTH_SHORT).show();
-
                                     Intent intent = new Intent(context, MainActivity.class);
                                     context.startActivity(intent);
                                     return true;
-
                                 } catch (SQLException e) {
                                     Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
                                     return false;
                                 }
                             }
                         }
-
                     }
                 }
             }
         }
         return false;
     }
-//************************ Funcion para borrar query en base de datos ******************************
+//*********************** Funcion para borrar productos en la base de datos *************************
     public boolean Borrar(int ID){
         boolean bandera;
         try{
@@ -204,6 +186,7 @@ public class Productos {
         return bandera;
     }
 
+    //************************* Retorna una Lista con todos los productos ***************************
     public ArrayList<Productos> mostrarProductos(){
 
         ArrayList<Productos> listaProductos = new ArrayList<Productos>();
@@ -211,14 +194,11 @@ public class Productos {
             Productos p;
             String query1 = "SELECT P.*,(SELECT COUNT(1) FROM LOTE L WHERE P.PRODUCTO_ID = L.PRODUCTO_ID) AS CANTIDADLOTES FROM PRODUCTO P ORDER BY PRODUCTO_FECHA_INGRESO DESC,PRODUCTO_ID DESC";
 
-
             ConnectionHelper conexion = new ConnectionHelper();
             con = conexion.connectionclass();
             Statement st = null;
-
             st = con.createStatement();
             ResultSet rs = st.executeQuery(query1);
-
 
             while (rs.next()) {
                p = new Productos();
@@ -227,12 +207,7 @@ public class Productos {
                p.setPRODUCTO_NOMBRE(rs.getString("PRODUCTO_NOMBRE"));
                p.setCantidadLotes(rs.getString("CANTIDADLOTES"));
                p.setPRODUCTO_FECHA_INGRESO(rs.getDate("PRODUCTO_FECHA_INGRESO"));
-            //   p.setPRODUCTO_FECHA_CADUCIDAD(rs.getDate("PRODUCTO_FECHA_CADUCIDAD"));
-            //   p.setPRODUCTO_NOTIFICACION_NARANJA(rs.getInt("PRODUCTO_NOTIFICACION_NARANJA"));
-            //    p.setPRODUCTO_NOTIFICACION_ROJA(rs.getInt("PRODUCTO_NOTIFICACION_ROJA"));
-               //url de la imagen
                listaProductos.add(p);
-               //Log.w("Producto: ",p.getPRODUCTO_ID() + " - " + p.getCATEGORIA_ID() + "-" + p.getPRODUCTO_NOMBRE());
             }
         }catch (SQLException e){
             System.out.println("Error in sql statment");
@@ -240,41 +215,27 @@ public class Productos {
         return listaProductos;
     }
 
-
-
-
-
+    //************************** Retorna Un producto en base a un ID ****************************
     public Productos verProducto(int id){
         Productos p = null;
         try {
-
             String query1 = "SELECT * FROM PRODUCTO WHERE PRODUCTO_ID = " + id;
-
             ConnectionHelper conexion = new ConnectionHelper();
             con = conexion.connectionclass();
             Statement st = null;
-
             st = con.createStatement();
             ResultSet rs = st.executeQuery(query1);
-
 
             while (rs.next()) {
                 p = new Productos();
                 p.setPRODUCTO_ID(rs.getInt("PRODUCTO_ID"));
                 p.setCATEGORIA_ID(rs.getInt("CATEGORIA_ID"));
                 p.setPRODUCTO_NOMBRE(rs.getString("PRODUCTO_NOMBRE"));
-                //p.setPRODUCTO_FECHA_CADUCIDAD(rs.getDate("PRODUCTO_FECHA_CADUCIDAD"));
-                //p.setPRODUCTO_NOTIFICACION_NARANJA(rs.getInt("PRODUCTO_NOTIFICACION_NARANJA"));
-                //p.setPRODUCTO_NOTIFICACION_ROJA(rs.getInt("PRODUCTO_NOTIFICACION_ROJA"));
-                //url de la imagen
-
-                //Log.w("Producto: ",p.getPRODUCTO_ID() + " - " + p.getCATEGORIA_ID() + "-" + p.getPRODUCTO_NOMBRE());
             }
         }catch (SQLException e){
             System.out.println("Error in sql statement");
         }
         return p;
     }
-
 
 }
